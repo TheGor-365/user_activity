66122055fb12cb1b67a9dc77d37083204b6374f6
Gor <7495500@gmail.com>
2023-04-16 14:28:10 +0300

    Bug fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
 
   def home
-    @activities = PublicActivity::Activity.where(owner_id: current_user.id).order('created_at DESC')
+    # @activities = PublicActivity::Activity.where(owner_id: current_user.id).order('created_at DESC')
+    @activities = PublicActivity::Activity.all
   end
 end
--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -1,6 +1,5 @@
 module ActivityHelper
   def action_name(activity)
-    puts activity.key
     t("public_activity.#{activity.key}")
   end
 end
